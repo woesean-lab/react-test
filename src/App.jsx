b6f03b55b4c0824a5e353d7556ef95da24581da1
Uğur Yorulmaz <woesean@gmail.com>
2025-12-29 00:00:53 +0300

    Fix missing useMemo import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { Toaster } from "react-hot-toast"
 import ProfileModal from "./components/modals/ProfileModal"
 import NoteModal from "./components/modals/NoteModal"
